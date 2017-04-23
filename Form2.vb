18f4e2406369e55d564f2fb3f7e9a4ba9be562d1
Deepsoumya <cdeepsoumyac@gmail.com>
2017-04-23 15:16:49 +0530

    Add files via upload

---
+++ Form2.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If ComboBox1.Text = "Very Easy" Then
+            Dim FILE_NAME As String = "C:\snake_py\VE.py"
+
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Process.Start(FILE_NAME)
+            Else
+                MsgBox("File Does Not Exist")
+            End If
+        ElseIf ComboBox1.Text = "Easy" Then
+            Dim FILE_NAME As String = "C:\snake_py\E.py"
+
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Process.Start(FILE_NAME)
+            Else
+                MsgBox("File Does Not Exist")
+            End If
+        ElseIf ComboBox1.Text = "Normal" Then
+            Dim FILE_NAME As String = "C:\snake_py\N.py"
+
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Process.Start(FILE_NAME)
+            Else
+                MsgBox("File Does Not Exist")
+            End If
+        ElseIf ComboBox1.Text = "Hard" Then
+            Dim FILE_NAME As String = "C:\snake_py\H.py"
+
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Process.Start(FILE_NAME)
+            Else
+                MsgBox("File Does Not Exist")
+            End If
+        ElseIf ComboBox1.Text = "Very Hard" Then
+            Dim FILE_NAME As String = "C:\snake_py\VH.py"
+
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Process.Start(FILE_NAME)
+            Else
+                MsgBox("File Does Not Exist")
+            End If
+        ElseIf ComboBox1.Text = "None" Then
+            MsgBox("Please Select any one Option")
+        End If
+    End Sub
+End Class
